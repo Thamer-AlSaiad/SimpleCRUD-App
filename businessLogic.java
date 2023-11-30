7b1af0dd068e5f5861c8983f14c7b0649de40634
Thamer-AlSaiad <139960208+Thamer-AlSaiad@users.noreply.github.com>
2023-11-30 11:59:43 -0400

    Update businessLogic.java
    Final edit of the code

--- businessLogic.java
+++ businessLogic.java
@@ -1,26 +1,41 @@
-public class businessLogic extends dbAccess{
-    public void pCreate(String pname,String price,String qty, String desc){
+
+public class businessLogic extends dbAccess {
+
+    public void pCreate(String pname, String price, String qty, String desc) {
         dbpInsert(pname, price, qty, desc);
     }
-    public void pUpdate(String name, String price, String qty, String desc, String id){
+
+    public void pUpdate(String name, String price, String qty, String desc, String id) {
         dbpUpdate(name, price, qty, desc, id);
     }
-    public void pDelete(String id){
+
+    public void pDelete(String id) {
         dbpDelete(id);
     }
-    public void uCreate(String uname,String upass,String uemail,String ucc,String uaddress){
+
+    public void uCreate(String uname, String upass, String uemail, String ucc, String uaddress) {
         dbuInsert(uname, upass, uemail, ucc, uaddress);
     }
-    public void uUpdate(String uname,String upass,String uemail,String ucc,String uaddress, String uid){
+
+    public void uUpdate(String uname, String upass, String uemail, String ucc, String uaddress, String uid) {
         dbuUpdate(uname, upass, uemail, ucc, uaddress, uid);
     }
-    public void uDelete(String uid){
+
+    public void uDelete(String uid) {
         dbuDelete(uid);
     }
-    public void gProdId(){
+
+    public void gProdId() {
         getpId();
     }
-    public void gUserId(){
+
+    public void gUserId() {
         getuId();
     }
+    public void pRead(javax.swing.JTable jT){
+        dbpRead(jT);
+    }
+     public void uRead(javax.swing.JTable jT){
+         dbuRead(jT);
+     }
 }
